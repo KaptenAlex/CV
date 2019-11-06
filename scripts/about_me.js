e46bf4e3e2a832b62f4a766694983f584930eb05
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-11-06 12:41:59 +0100

    Margin to image, changes to secret and added alt attr.

--- scripts/about_me.js
+++ scripts/about_me.js
@@ -1,7 +1,9 @@
 $(function(){
   $("#showText").hover(function(){
+    $(".secretText").fadeIn(1000);
     $(".secretText").attr("style", "display:inline");
   }, function(){
+    $(".secretText").fadeOut(500);
     $(".secretText").attr("style", "display:none");
   });
 });

ff397db896beebe37d29fabbe0f8bb5eb242406a
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-11-08 13:32:16 +0100

    replaced cv with resume.

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,27 +1,27 @@
 $(function() {
   let portfolioDiv = $("#portfolio");
   let aboutMeDiv = $("#about_me");
-  let cvDiv = $("#cv");
+  let resumeDiv = $("#resume");
   //Function to switch active state on navbar links.
   $(".nav-link").click(function() {
     let currentObject = $(this);
     console.log(currentObject.attr("id"));
     $(".nav-link").attr("class", "nav-link");
     currentObject.attr("class", "nav-link active");
-    if(currentObject.attr("id") == "cvLink"){
-      cvDiv.attr("style", "display:block");
+    if(currentObject.attr("id") == "resumeLink"){
+      resumeDiv.attr("style", "display:block");
       aboutMeDiv.attr("style", "display:none");
       portfolioDiv.attr("style", "display:none;");
     }
     else if (currentObject.attr("id") == "about_meLink") {
       aboutMeDiv.attr("style", "display:block");
-      cvDiv.attr("style", "display:none");
+      resumeDiv.attr("style", "display:none");
       portfolioDiv.attr("style", "display:none;");
     }
     else if (currentObject.attr("id") == "portfolioLink") {
       portfolioDiv.attr("style", "display:block;");
       aboutMeDiv.attr("style", "display:none");
-      cvDiv.attr("style", "display:none");
+      resumeDiv.attr("style", "display:none");
     }
   });
 });

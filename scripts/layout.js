03ef7a7ba86dfcf0d0577a00e31cc2f9f37b20e6
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-11-13 12:23:14 +0100

    Made links more friendly for the programmer.

--- scripts/layout.js
+++ scripts/layout.js
@@ -5,8 +5,9 @@ $(function() {
   //Function to switch active state on navbar links.
   $(".nav-link").click(function() {
     let currentObject = $(this);
-    $(".nav-link").attr("class", "nav-link");
-    currentObject.attr("class", "nav-link active");
+    $(".nav-link").removeClass("active");
+    currentObject.addClass("active");
+
     if(currentObject.attr("id") == "resumeLink"){
       resumeDiv.attr("style", "display:block");
       aboutMeDiv.attr("style", "display:none");
